608457d5a231be33ea60983debea4d49dccbc6a2
Anton Wahrstätter <51536394+Nerolation@users.noreply.github.com>
2021-10-04 11:29:46 +0200

    Delete blockchain_parser/tests directory

--- blockchain_parser/tests/utils.py
+++
@@ -1,9 +0,0 @@
-import os
-from binascii import a2b_hex
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-
-
-def read_test_data(filename):
-    with open(os.path.join(dir_path, "data/", filename)) as f:
-        return a2b_hex(f.read().strip())
