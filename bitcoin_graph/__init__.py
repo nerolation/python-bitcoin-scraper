561ba62aecf41ccffe3c32ca9b52e238c59178ba
Anton Wahrstätter <51536394+Nerolation@users.noreply.github.com>
2021-10-06 18:46:22 +0200

    Add files via upload

--- bitcoin_graph/__init__.py
+++ bitcoin_graph/__init__.py
@@ -7,18 +7,20 @@ from termcolor import colored
 __version__ = "0.1.0"
 __cwd__     = os.path.abspath(os.getcwd())
 
-def starting(args):
+def starting_info(args):
     print(f"Starting btc graph version {__version__} with the following arguments:")
-    print("{:<18}{:<13}".format("Current wd:", __cwd__))
+    print("{:<18}{:<13}".format("current wd:", __cwd__))
+    if str(args["rawedges"]) not in ["False", "None", "0"]:
+        args["format"] = colored("deactivated", "red")
     for k, v in zip(args.keys(), args.values()):
-        if v == True:
+        if (v and k not in ["startfile","blklocation","format"]):
             v = colored("activated", "green")
-        else:
+        elif k not in ["startfile","blklocation","format"]:
             v = colored("deactivated", "red")
         print("{:<18}{:<13}".format(k+":", str(v)))
     r = 40
     p = lambda x: "\r[" + "#" * x + (r - x) * " " + "]"
-    print("Initializing...")
+    print("\nInitializing...")
     for i in range(r):
         sys.stdout.write(p(i+1))
         time.sleep(0.01)
