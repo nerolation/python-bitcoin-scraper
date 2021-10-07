eb905b7b69009af56de17f48ae88245ebb58e562
Anton Wahrstätter <51536394+Nerolation@users.noreply.github.com>
2021-10-07 09:28:42 +0200

    Add files via upload

--- run.py
+++ run.py
@@ -4,16 +4,16 @@ from networkit import *
 import argparse
 import time
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-sf', '--startfile', default="blk00000.dat")
-parser.add_argument('-ef', '--endfile', default=None)
-parser.add_argument('-st', '--starttx', default=None)
-parser.add_argument('-et', '--endtx', default=None)
-parser.add_argument('-ets', '--endtimestamp', default=None)
-parser.add_argument('-loc', '--blklocation', default="~/.bitcoin/blocks")
-parser.add_argument('-f', '--format', default="binary")
-parser.add_argument('-raw', '--rawedges', default=None)
-parser.add_argument('-p', '--printing', default=True)
+parser = argparse.ArgumentParser(formatter_class=lambda prog: argparse.HelpFormatter(prog,max_help_position=48))
+parser.add_argument('-sf', '--startfile', help=".blk start file", default="blk00000.dat")
+parser.add_argument('-ef', '--endfile', help=".blk end file (excluded)", default=None)
+parser.add_argument('-st', '--starttx', help="start transaction", default=None)
+parser.add_argument('-et', '--endtx', help="end transaction", default=None)
+parser.add_argument('-ets', '--endts', help="end timestamp of block", default=None)
+parser.add_argument('-loc', '--blklocation', help=".blk file location", default="~/.bitcoin/blocks")
+parser.add_argument('-f', '--format', help="networkit storage format (binary|edgelist)", default="binary")
+parser.add_argument('-raw', '--rawedges', help="only build list of edges", default=None)
+parser.add_argument('-wt', '--withts', help="collect list of edges with timestamps", default=None)
 
 # Handle parameters
 _args = parser.parse_args()
@@ -26,18 +26,18 @@ startFile = _args.startfile
 endFile   = _args.endfile
 startTx   = _args.starttx
 endTx     = _args.endtx
-endTS     = _args.endtimestamp
+endTS     = _args.endts
 blk_loc   = _args.blklocation
 _format   = _args.format
 rawEdges  = _args.rawedges
-printing  = _args.printing
+withTS    = _args.withts
 # -----------------------------------------------
 
 
 # Initialize Btc graph object
 # `blk_loc` for the location where the blk files are stored
 # `raw Edges` to additionally save graph in EdgeList format
-btc_graph = BtcGraph(dl=blk_loc, endTS=endTS, graphFormat=_format, buildRawEdges=rawEdges, printing=printing)
+btc_graph = BtcGraph(dl=blk_loc, endTS=endTS, graphFormat=_format, buildRawEdges=rawEdges, withTS=withTS)
 
 # Start building graph
 btc_graph.build(startFile,endFile,startTx,endTx)
