19a6c2fbfffd0d0eb48a503222fe7980d7712532
Anton Wahrstätter <51536394+Nerolation@users.noreply.github.com>
2021-10-14 13:02:04 +0200

    Add files via upload

--- run.py
+++ run.py
@@ -16,6 +16,7 @@ parser.add_argument('-et', '--endtx', help="end transaction - default: None", de
 parser.add_argument('-ets', '--endts', help="end timestamp of block - default: None", default=None)
 parser.add_argument('-loc', '--blklocation', help=".blk|.csv file location - default: ~/.bitcoin/blocks", default="~/.bitcoin/blocks")
 parser.add_argument('-f', '--format', help="networkit storage format (binary|edgelist) - default: binary", default="binary")
+parser.add_argument('-utxo', '--uxtos', help="path to existing Utxos file - default: None", default="None")
 
 # Raw edge-list
 parser.add_argument('-raw', '--rawedges', help="path to store raw edges - default: None", default=None)
@@ -52,12 +53,13 @@ endTx     = _args.endtx
 endTS     = _args.endts
 file_loc  = _args.blklocation
 _format   = _args.format
+utxos     = _args.utxos
 rawEdges  = _args.rawedges
 withTS    = _args.withts
 gbq       = _args.googlebigquery
 upload    = _args.directupload
 creds     = _args.credentials
-table_id   = _args.tableid
+table_id  = _args.tableid
 dataset   = _args.dataset
 # -----------------------------------------------
 
@@ -65,7 +67,8 @@ if not gbq:
     # Initialize btc graph object
     # `blk_loc` for the location where the blk files are stored
     # `raw Edges` to additionally save graph in edgeList format
-    btc_graph = BtcGraph(dl=file_loc, endTS=endTS, graphFormat=_format, buildRawEdges=rawEdges, withTS=withTS, upload=upload,
+    btc_graph = BtcGraph(dl=file_loc, Utxos=utxos, endTS=endTS, graphFormat=_format,
+                        buildRawEdges=rawEdges, withTS=withTS, upload=upload,
                         credentials=creds, table_id=table_id, dataset=dataset)
 
     # Start building graph
