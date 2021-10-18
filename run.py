bcd7f5978a1cfd988379309b045bb90df7d7fc0b
Anton Wahrstätter <51536394+Nerolation@users.noreply.github.com>
2021-10-18 12:28:55 +0200

    Add files via upload

--- run.py
+++ run.py
@@ -11,13 +11,13 @@ import os
 parser = argparse.ArgumentParser(formatter_class=lambda prog: argparse.HelpFormatter(prog,max_help_position=60))
 parser.add_argument('-sf', '--startfile', help=".blk start file - default: blk00000.dat", default="blk00000.dat")
 parser.add_argument('-ef', '--endfile', help=".blk end file (excluded) - default: None", default=None)
-parser.add_argument('-st', '--starttx', help="start transaction - default: None", default=None)
-parser.add_argument('-et', '--endtx', help="end transaction - default: None", default=None)
+parser.add_argument('-st', '--starttx', help="start transaction (included) - default: None", default=None)
+parser.add_argument('-et', '--endtx', help="end transaction (excluded) - default: None", default=None)
 parser.add_argument('-ets', '--endts', help="end timestamp of block - default: None", default=None)
 parser.add_argument('-loc', '--blklocation', help=".blk|.csv file location - default: ~/.bitcoin/blocks", default="~/.bitcoin/blocks")
 parser.add_argument('-f', '--format', help="networkit storage format (binary|edgelist) - default: binary", default="binary")
 parser.add_argument('-utxo', '--utxos', help="path to existing Utxos file - default: None", default=None)
-parser.add_argument('-lm', '--lowmemory', help="low memory mode - default: False", default=None)
+parser.add_argument('-lm', '--lowmemory', help="low memory mode (collecting raw tx inputs) - default: False", default=None)
 
 
 # Raw edge-list
